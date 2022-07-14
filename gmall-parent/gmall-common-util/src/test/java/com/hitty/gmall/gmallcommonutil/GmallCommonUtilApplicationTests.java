95bc4d1ada7252036e268a6a523cd78589804562
hittey <lianrong7@163.com>
2022-07-14 22:07:07 +0800

    创建公共工具模块

---
+++ gmall-parent/gmall-common-util/src/test/java/com/hitty/gmall/gmallcommonutil/GmallCommonUtilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hitty.gmall.gmallcommonutil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmallCommonUtilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
